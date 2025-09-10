bbcd33caf8fc74f45b81af688a66e5226e1aa002
bureaucracy-web <bureaucracy789@gmail.com>
2025-09-10 13:11:28 +0000

    viteConfiged

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
+    host: "127.0.0.1",
+    allowedHosts: ["admin.test.communistmuseum.org"], // allow this host
+    proxy: {
+      "/api": {
+        target: "https://backend.test.communistmuseum.org",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
